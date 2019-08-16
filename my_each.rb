c3bdfc00a5568c83afd29e0fee4573dd457fd152
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-08-16 13:54:58 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,4 @@
 def my_each(array)# put argument(s) here
-  if block_given?
-    i = 0 
-    while i < array.lenght
-      yield (array[i])
-      i = i + 1 
-    end
-    array
-    
-  end
+  while i < array.length
   # code here
 end
